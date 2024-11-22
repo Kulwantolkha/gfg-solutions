b4a15abb5c9435f30140647db34b74561ab660a8
Kulwantolkha <114793930+Kulwantolkha@users.noreply.github.com>
2024-11-22 22:51:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -8,15 +8,58 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int countFreq(vector<int>& arr, int target) {
-        // code here
-        int cnt = 0;
-        for(int i=0;i<arr.size();i++){
-            if(arr[i]==target){
-                cnt++;
+    int firstOccurance(vector<int>& arr,int target){
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]>target){
+                high = mid-1;
+            }
+            else if(arr[mid]<target){
+                low = mid+1;
+            }
+            else{
+                if(mid==0 || arr[mid]!=arr[mid-1]){
+                    return mid;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
-        return cnt;
+        return -1;
+    }
+    int lastOccurance(vector<int>& arr,int target){
+        int low = 0;
+        int high = arr.size()-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid]<target){
+                low = mid+1;
+            }
+            else if(arr[mid]>target){
+                high = mid-1;
+            }
+            else{
+                if( mid==0 || arr[mid]!=arr[mid+1]){
+                    return mid;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+    int countFreq(vector<int>& arr, int target) {
+        // code here
+        int x = firstOccurance(arr,target);
+        int y = lastOccurance(arr,target);
+        if(x==-1){
+            return 0;
+        }
+        else    return y-x+1;
     }
 };
 
